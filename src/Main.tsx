0d2a7f9103fcd8040a19dce21bf6f900d213de6a
EduardMatei0 <eduardmatei0@yahoo.com>
2023-01-06 18:30:48 +0200

    add login + sign up page + react router

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,55 +4,29 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import NavBar from "./components/NavBar";
-import PeoplePage from "./pages/PeoplePage";
-import MainTitle from "./components/MainTitle";
-import {CssBaseline} from "@mui/material";
-import {Toaster} from "react-hot-toast";
-import DepartmentPage from "./pages/DepartmentPage";
-import usePeople from "./hooks/usePeople";
-import {PeopleContext} from "./context/PeopleContext";
-import useDepartments from "./hooks/useDepartments";
-import {FilterForm} from "./model/FilterForm";
-import usePeopleSearch from "./hooks/usePeopleSearch";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignUpPage />,
+    },
+]);
 
-const Main = () => {
-    const [value, setValue] = useState('1');
-    const [applyFilter, setApplyFilter] = useState<boolean>( false);
-    const [filterForm, setFilterForm] = useState<FilterForm>({
-        names: [],
-        emails: [],
-        phoneNumbers: [],
-        departments: [],
-        categories: []
-    });
-    const [peopleList, setPeopleList] = usePeopleSearch(applyFilter, filterForm);
-    const [allPeople] = usePeople();
-    const [departments, setDepartments] = useDepartments();
-    return (
-    <div className="App">
-        <PeopleContext.Provider value={allPeople}>
-            <CssBaseline />
-            <Toaster />
-            <header >
-                <MainTitle />
-                <NavBar value={value} setValue={setValue} />
-                {value === "1" && (<PeoplePage
-                    peopleList={peopleList}
-                    setPeopleList={setPeopleList}
-                    departments={departments}
-                    applyFilter={applyFilter}
-                    setApplyFilter={setApplyFilter}
-                    filterForm={filterForm}
-                    setFilterForm={setFilterForm}
-                />)}
-                {value === "2" && (<DepartmentPage departments={departments} setDepartments={setDepartments}/>)}
-            </header>
-        </PeopleContext.Provider>
-    </div>
-  );
-}
+const Main = () => (
+    <RouterProvider router={router}/>
+)
 
 export default Main;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import {FilterForm} from "../model/FilterForm";
+import usePeopleSearch from "../hooks/usePeopleSearch";
+import usePeople from "../hooks/usePeople";
+import useDepartments from "../hooks/useDepartments";
+import {PeopleContext} from "../context/PeopleContext";
+import {CssBaseline} from "@mui/material";
+import {Toaster} from "react-hot-toast";
+import MainTitle from "../components/MainTitle";
+import NavBar from "../components/NavBar";
+import PeoplePage from "./PeoplePage";
+import DepartmentPage from "./DepartmentPage";
+
+
+const HomePage = () => {
+    const [value, setValue] = useState('1');
+    const [applyFilter, setApplyFilter] = useState<boolean>( false);
+    const [filterForm, setFilterForm] = useState<FilterForm>({
+        names: [],
+        emails: [],
+        phoneNumbers: [],
+        departments: [],
+        categories: []
+    });
+    const [peopleList, setPeopleList] = usePeopleSearch(applyFilter, filterForm);
+    const [allPeople] = usePeople();
+    const [departments, setDepartments] = useDepartments();
+    return (
+        <div className="App">
+            <PeopleContext.Provider value={allPeople}>
+                <CssBaseline />
+                <Toaster />
+                <header >
+                    <MainTitle />
+                    <NavBar value={value} setValue={setValue} />
+                    {value === "1" && (<PeoplePage
+                        peopleList={peopleList}
+                        setPeopleList={setPeopleList}
+                        departments={departments}
+                        applyFilter={applyFilter}
+                        setApplyFilter={setApplyFilter}
+                        filterForm={filterForm}
+                        setFilterForm={setFilterForm}
+                    />)}
+                    {value === "2" && (<DepartmentPage departments={departments} setDepartments={setDepartments}/>)}
+                </header>
+            </PeopleContext.Provider>
+        </div>
+    );
+};
+
+export default HomePage;
